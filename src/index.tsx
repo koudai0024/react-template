bcace170a331685583111c487651932826615ddd
koudai0024 <iwai.0731v@gmail.com>
2021-05-10 21:51:38 +0900

    ✨ ルーティングの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import * as ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "src/pages/Home";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <div id="app">
-    <Home />
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   </div>,
   rootElement
 );
